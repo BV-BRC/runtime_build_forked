c735c0b071ef7a149a192adc6b8f1defdc55357d
vagrant <vagrant@localhost.localdomain>
2016-11-27 17:00:39 -0600

    added bison build to thrift build

---
+++ kb_thrift_runtime/bison_build.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+set -x
+
+target=${TARGET-/kb/runtime}
+
+if [[ $# -gt 0 ]] ; then
+        target=$1
+        shift
+fi
+
+
+wget http://ftp.gnu.org/gnu/bison/bison-2.5.1.tar.gz
+tar xvf bison-2.5.1.tar.gz
+cd bison-2.5.1
+./configure --prefix=$target
+make
+sudo make install
+cd ..
