9feea4f5c5dae7cceed7a86e7f3fcc830216ca03
Robert Olson <olson@mcs.anl.gov>
2018-09-17 10:41:04 -0500

    Mods

--- kb_prok_tuxedo/install_prok_tuxedo.sh
+++ kb_prok_tuxedo/install_prok_tuxedo.sh
@@ -2,11 +2,17 @@
 
 target=${TARGET-/kb/runtime}
 
+if [ $# -gt 0 ] ; then
+	target=$1
+fi
+
 #wget --no-check-certificate -P ${target}/bin https://github.com/aswarren/Prok-tuxedo/raw/master/prok_tuxedo.py
 rm -rf Prok-tuxedo
 git clone https://github.com/aswarren/Prok-tuxedo.git
-cp Prok-tuxedo/prok_tuxedo.py ${target}/bin/
-cp Prok-tuxedo/cuffdiff_to_genematrix.py ${target}/bin/
+cd Prok-tuxedo
+git checkout contrasts
+cp prok_tuxedo.py ${target}/bin/
+cp cuffdiff_to_genematrix.py ${target}/bin/
 
 chmod a+x ${target}/bin/prok_tuxedo.py
 chmod a+x ${target}/bin/cuffdiff_to_genematrix.py
