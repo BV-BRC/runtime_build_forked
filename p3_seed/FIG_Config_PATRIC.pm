ccc9b8ede91495715ab79bd1d7a195a982b3e0e6
Robert Olson <olson@mcs.anl.gov>
2019-09-05 15:16:07 -0500

    SEED and SEEDtk builders.

---
+++ p3_seed/FIG_Config_PATRIC.pm
@@ -0,0 +1,15 @@
+package FIG_Config;
+$dbms = "mysql";
+$dbuser = "seed";
+$db = "fig_public_pseed";
+$dbhost = "seed-db-write.mcs.anl.gov";
+
+$try_sim_server = 1;
+$sim_server_url = "http://bio-data-2.mcs.anl.gov/simserver/perl/sims.pl";
+$use_bbh_server = 1;
+$bbh_server_url = "http://sim-server.nmpdr.org/simserver/perl/bbhs.pl";
+$pch_server_url = "http://sim-server.nmpdr.org/simserver/perl/pchs.pl";
+$use_pch_server = 1;
+
+
+1;
